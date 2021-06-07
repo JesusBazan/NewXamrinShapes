19fac5fadc0e4789d5879807ae50d957b92ff94a
EmmanuelBazan <mqkz99@gmail.com>
2021-06-06 22:52:58 -0500

    Add project files.

---
+++ NewXamrinShapes/NewXamrinShapes/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NewXamrinShapes.View;
+
+namespace NewXamrinShapes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Device.SetFlags(new[] { "Shapes_Experimental", "Brush_Experimental" });
+            InitializeComponent();
+            MainPage = new NavigationPage(new Menu1());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ NewXamrinShapes/NewXamrinShapes/View/Menu1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewXamrinShapes.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu1 : ContentPage
+    {
+        public Menu1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var rectangulo = new Rectangulo();
+            Navigation.PushModalAsync(rectangulo);
+            NavigationPage.SetHasNavigationBar(rectangulo, true);
+            NavigationPage.SetHasBackButton(rectangulo, true);
+            
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            //Elipse
+            Navigation.PushModalAsync(new Elipse());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Linea());
+        }
+    }
+}
